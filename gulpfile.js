1a7b375941184ce14e38fbd902fb16f579f24f81
Matt <mhooper72@gmail.com>
2018-03-16 17:52:41 -0600

    master start

---
+++ gulpfile.js
@@ -0,0 +1,37 @@
+const gulp = require('gulp'),
+	path = require('path'),
+	sass = require('gulp-sass'),
+	autoprefixer = require('gulp-autoprefixer'),
+	sourcemaps = require('gulp-sourcemaps'),
+	open = require('gulp-open');
+
+var Paths = {
+	HERE: './',
+	DIST: './dist/',
+	CSS: './assets/css/',
+	SCSS_TOOLKIT_SOURCES: './assets/scss/Optfit.scss',
+	SCSS: './assets/scss/**/**',
+	IMG: './assets/img/**/**',
+	JS: './assets/js/*.js'
+};
+
+gulp.task('compile-scss', function() {
+	return gulp
+		.src(Paths.SCSS_TOOLKIT_SOURCES)
+		.pipe(sourcemaps.init())
+		.pipe(sass().on('error', sass.logError))
+		.pipe(autoprefixer())
+		.pipe(sourcemaps.write(Paths.HERE))
+		.pipe(gulp.dest('./dist/css'));
+});
+
+gulp.task('watch', function() {
+	gulp.watch(Paths.SCSS, ['compile-scss']);
+});
+
+gulp.task('open', function() {
+	gulp.src('index.html').pipe(open());
+});
+
+gulp.task('open-app', ['open', 'watch']);
+gulp.task('prod', ['compress', 'compile-scss']);
